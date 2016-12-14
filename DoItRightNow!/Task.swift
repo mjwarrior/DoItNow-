6719960737bbad3fbca41559bbf218a11b85516d
marcstreeter <mstreeter@gmail.com>
2016-12-14 14:21:08 -0600

    I finished lecture 58 section 6 I added a exclamation point ❗️ and function called makeTasks

---
+++ DoItRightNow!/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  DoItRightNow!
+//
+//  Created by Marc Streeter on 12/14/16.
+//  Copyright © 2016 Developer Warriors. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+
+var name = ""
+var important = false
+    
+    
+}
--- DoItRightNow!/ViewController.swift
+++ DoItRightNow!/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-        
+        tasks = makeTasks()
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -27,16 +29,41 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       let cell = UITableViewCell()
-      cell.textLabel?.text = "Hello"
+      let task = tasks[indexPath.row]
+        if task.important{
+         cell.textLabel?.text = "❗️\(task.name)"
+        }else{
+         cell.textLabel?.text = task.name
+        }
+        
       return cell  
     }
 
+    func makeTasks() -> [Task] {
+       let task1 = Task()
+       task1.name = "learn programing"
+        task1.important = true
+        
+        let task2 = Task()
+        task2.name = "learn spanish"
+        task2.important = false
+        
+        let task3 = Task()
+        task3.name = "make sure nean dosen't eat my dessert "
+        task3.important = true
+        
+        return [task1,task2,task3]
+        
 
-
+    }
+    
+    
 }
+
+
 
